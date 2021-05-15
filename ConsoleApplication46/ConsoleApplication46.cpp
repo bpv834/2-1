b2be94d77b2189edc3f35781a41b4e41f7a2a5a0
bpv834 <81566093+bpv834@users.noreply.github.com>
2021-05-16 02:40:46 +0900

    초기 커밋.

---
+++ ConsoleApplication46/ConsoleApplication46.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+
+int main() {
+	char books[5][30] = {"wonder","me before you","the hunger game","twilight","harry poter",};
+	int i = 0;
+
+	for (i; i < 5; i++)
+		printf("%s\n", books[i]);
+
+	for (i=0; i < 5; i++) {
+		if (islower(books[i][0]))
+			books[i][0] = toupper(books[i][0]);
+	}
+	puts("<<변경 후>>");
+	for (i=0; i < 5; i++)
+		printf("%s\n", books[i]);
+
+}
